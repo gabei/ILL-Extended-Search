fe2f67c6a6546ed91b6f3e49755e8f3fd355655f
gabei <gabe1792@gmail.com>
2025-05-22 18:35:17 -0500

    add guiding comment for future me

--- login/worldcat-login.js
+++ login/worldcat-login.js
@@ -5,3 +5,12 @@ import { worldCatConfig as config} from './config.js';
 const browser = await puppeteer.launch({headless: false});
 const page = await browser.newPage();
 page.setDefaultNavigationTimeout(10000); // 10 seconds
+
+
+
+
+
+// list of library names narrowed down to their text:
+// let names = document.querySelectorAll('ul[data-testid="holding-list-details"] li strong')
+// Array.from(names)
+// this works in the console to single them into one HTML node list
