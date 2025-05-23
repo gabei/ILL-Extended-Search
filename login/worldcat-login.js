7f99a0b2dbb568a8ca44144a35f64c6f9f4b6841
gabei <gabe1792@gmail.com>
2025-05-23 10:54:50 -0500

    Comments: Left guiding comments for future page crawling

--- login/worldcat-login.js
+++ login/worldcat-login.js
@@ -8,7 +8,43 @@ page.setDefaultNavigationTimeout(10000); // 10 seconds
 
 
 
+// navigate to email input page
+
+
+// on email input page
+// select input type="text" and fill with worldcat username
+// select button type=submit and click 
+// (this takes us to the actual login page)
+
+
+// on login page
+// select input type="text" again and fill with worldcat username
+// select input type="password" and fill with password
+// select button type=submit and click 
+// *may need to retry after 403 from WorldCat?
+
+
+// on main search page
+// select input id="home-page-search-box" and fill with ISBN
+// select button type=submit and click 
+
+
+// on search results page
+// select first h2 > a and click
+
+
+// on book page
+// select button with text "All libraries"
+// select li with data-value="50" and click
+// wait
+//  if div with data-testid="holdings-error-notification-message"
+//    restart process
+// 
+
 // list of library names narrowed down to their text:
 // let names = document.querySelectorAll('ul[data-testid="holding-list-details"] li strong')
 // Array.from(names)
 // this works in the console to single them into one HTML node list
+
+
+// create that list of libraries in an array to search in the AGExternal json list
