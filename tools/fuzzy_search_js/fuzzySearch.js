662f661a38c0103ee2627e1b5b105fbe1a2a36e8
gabei <gabe1792@gmail.com>
2025-06-06 13:55:35 -0500

    Chore: Begin file setup for javscript based fuzzy search

---
+++ tools/fuzzy_search_js/fuzzySearch.js
@@ -0,0 +1,26 @@
+import {fuzzball as fuzz} from "fuzzball";
+
+
+const normalizeString = (str) => {
+
+}
+
+const normalizeLibraryNames = (libraryNames) => {
+
+}
+
+const createLenderCodeDictionary = (lenderCodes) => {
+
+}
+
+const runNameMatchSearch = (data, lenders, matches) => {
+
+}
+
+const removeDuplicateEntries = (matches) => {
+
+}
+
+const printMatchList = (matches) => {
+
+}
