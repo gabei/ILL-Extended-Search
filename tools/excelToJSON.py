895a20f276797bf234ca092a597f98c04901fbad
gabei <gabe1792@gmail.com>
2025-06-06 14:52:32 -0500

    Fix: Remove Column from AGExternal data that was causing JSON errors and import json into new js based fuzzy search script

--- tools/excelToJSON.py
+++ tools/excelToJSON.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import json
 
-data = pd.read_excel('AGExternal.xlsx', sheet_name="ALL AGEXTERNAL")
+data = pd.read_excel('AGExternal.xlsx', sheet_name="Lenders to TX")
 json_data = data.to_dict(orient='records')
+for entry in json_data:
+    del entry["OCLC SYMBOL"]
 
-with open('ALLlenders.json', 'w') as json_file:
-    json.dump(json_data, json_file, indent=4)
+with open('fuzzy_search_js/TXlenders.json', 'w') as json_file:
+    json.dump(json_data, json_file, indent=4, allow_nan=True)
 
 json_file.close()
--- tools/fuzzy_search_js/fuzzySearch.js
+++ tools/fuzzy_search_js/fuzzySearch.js
@@ -1,26 +1,48 @@
-import {fuzzball as fuzz} from "fuzzball";
+//import fuzz from "fuzzball";
+import TXlenders from "./TXlenders.json" with {type: 'json'};
+import ALLlenders from "./ALLlenders.json" with {type: 'json'};
+
 
 
 const normalizeString = (str) => {
-
+  return str.toLowerCase();
 }
+
+
 
 const normalizeLibraryNames = (libraryNames) => {
-
+  return libraryNames.map(name => normalizeString(name));
 }
+
+
 
 const createLenderCodeDictionary = (lenderCodes) => {
+  const dict = new Object();
 
+  for(const key in lenderCodes){
+    dict[normalizeString(key)] = lenderCodes[key];
+  }
+
+  console.log(dict);
+  return dict;
 }
+
+
 
 const runNameMatchSearch = (data, lenders, matches) => {
 
 }
 
+
+
 const removeDuplicateEntries = (matches) => {
 
 }
 
+
+
 const printMatchList = (matches) => {
 
 }
+
+createLenderCodeDictionary(TXlenders);
