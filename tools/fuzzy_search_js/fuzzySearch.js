382eeb3558c2f21a30ccc183228948d5cef80d3e
gabei <gabe1792@gmail.com>
2025-06-09 17:15:16 -0500

    Refactor: Adding methods from python search to js search

--- tools/fuzzy_search_js/fuzzySearch.js
+++ tools/fuzzy_search_js/fuzzySearch.js
@@ -2,28 +2,54 @@
 import TXlenders from "./TXlenders.json" with {type: 'json'};
 import ALLlenders from "./ALLlenders.json" with {type: 'json'};
 
-
+const searchable_libraries = [
+  'Ardmore Public Library',
+  'Springfield-Greene County Library District',
+  'Mid-Continent Public Library',
+  'Falls City Library and Arts Center',
+  'Saint Louis Public Library',
+  'Henderson County Public Library District',
+  'Johnson County Public Library',
+  'Carnegie Stout Public Library',
+  'Allen County Public Library',
+  'Hennepin County Library',
+  'K.O. Lee Aberdeen Public Library',
+  'Stark County District Library',
+  'Los Angeles Public Library',
+  'Sacramento Public Library',
+  'New York Public Library',
+  'King County Library System',
+  'Auckland Libraries',
+  'Hutt City Libraries',
+  'Brisbane City Council Library Services',
+  'Mackay Regional Council',
+  'Hills Shire Library Service',
+  'Springvale Library and Information Service'
+]
 
 const normalizeString = (str) => {
-  return str.toLowerCase();
+  return String(str).toLowerCase();
 }
 
 
 
 const normalizeLibraryNames = (libraryNames) => {
-  return libraryNames.map(name => normalizeString(name));
+  return libraryNames.map((name) => {
+    return normalizeString(name);
+  });
 }
 
 
 
-const createLenderCodeDictionary = (lenderCodes) => {
-  const dict = new Object();
+const createLenderCodeDict = (lenderCodes) => {
+  let dict = lenderCodes.map((item) => {
+    return {
+      name: normalizeString(item["LIBRARY NAME"]),
+      code: item["AGEXTERNAL CODE"],
+    };
+  });
 
-  for(const key in lenderCodes){
-    dict[normalizeString(key)] = lenderCodes[key];
-  }
 
-  console.log(dict);
   return dict;
 }
 
@@ -45,4 +71,5 @@ const printMatchList = (matches) => {
 
 }
 
-createLenderCodeDictionary(TXlenders);
+//console.log(createLenderCodeDict(TXlenders));
+// console.log(normalizeLibraryNames(searchable_libraries));
