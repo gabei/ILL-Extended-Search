6345e545da86d411921f5be6286fe6b4c802068d
gabei <gabe1792@gmail.com>
2025-06-10 16:51:10 -0500

    change port

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 import initWorldcat from './login/worldcat/worldcat-login.js';
-
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from "cors";
 dotenv.config();
 
+
 const app = express();
 const port = process.env.PORT;
+
+
 app.use(cors());
 app.use(express.json());
 
-app.listen(port || 3000, () => {
+
+app.listen(port || 8000, () => {
   console.log("App listening on port " + port);
 })
+
 
 app.get("/search", async (req, res)=> {
   const ISBN = req.query.code
