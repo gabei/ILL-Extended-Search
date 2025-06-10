caf1325ae0c901f02b4b154fbdd69c6fee3f727f
gabei <gabe1792@gmail.com>
2025-06-10 13:30:30 -0500

    Setup basic express server

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-// import initWorldCat  from "./login/worldcat/worldcat-login.js";
-// initWorldCat();
 
 
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from "cors";
 dotenv.config();
 
+const port = process.env.PORT;
 
+const app = express();
+app.use(cors());
 
+app.listen(port || 3000, () => {
+  console.log("App listening on port " + port);
+})
+
+app.get("/", (err, req, res)=> {
+  res.send("You hit the home page!");
+});
