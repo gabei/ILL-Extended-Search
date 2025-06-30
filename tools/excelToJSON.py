a66fd67db382aabdab036c72647069b9a64dd049
gabei <gabe1792@gmail.com>
2025-06-30 10:15:18 -0500

    Update lender list

--- tools/excelToJSON.py
+++ tools/excelToJSON.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import json
 
-data = pd.read_excel('AGExternal.xlsx', sheet_name="Lenders to TX")
+data = pd.read_excel('AGExternal.xlsx', sheet_name="ALL AGEXTERNAL")
 json_data = data.to_dict(orient='records')
 for entry in json_data:
     del entry["OCLC SYMBOL"]
 
-with open('fuzzy_search_js/TXlenders.json', 'w') as json_file:
+with open('fuzzy_search_js/ALLlenders.json', 'w') as json_file:
     json.dump(json_data, json_file, indent=4, allow_nan=True)
 
 json_file.close()
