3b7cf94243768170d1a08d3774342146b20ddd6f
gabei <gabe1792@gmail.com>
2025-08-05 16:02:46 -0500

    Update browser timeout

--- login/general/browser.js
+++ login/general/browser.js
@@ -7,7 +7,7 @@ const browser = await puppeteer.launch(browserOptions);
 
 
 let page = await browser.newPage()
-  page.setDefaultNavigationTimeout(20000); // 3 minutes
+  page.setDefaultNavigationTimeout(120000); // 2 minutes
   page.setCacheEnabled(false);
   page.setUserAgent(await getRandomUserAgent());
 
