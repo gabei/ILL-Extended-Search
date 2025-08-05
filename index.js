f10f76e3c50e41d5275b2c9f3509d41018a25f48
gabei <gabe1792@gmail.com>
2025-08-05 14:28:14 -0500

    Use manual cors headers instead of cors package

--- index.js
+++ index.js
@@ -8,14 +8,26 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-const corsOptions = {
-  origin: ['https://gabei.github.io/LTCL-client/',
-    'http://gabei.github.io/LTCL-client/',
-  'https://gabei.github.io',
-  'http://gabei.github.io'],
-  methods: ['GET', 'POST'],
-}
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: ['https://gabei.github.io/LTCL-client/',
+//     'http://gabei.github.io/LTCL-client/',
+//   'https://gabei.github.io',
+//   'http://gabei.github.io'],
+//   methods: ['GET', 'POST'],
+// }
+// app.use(cors(corsOptions));
+
+
+app.use(req, res, next => {
+  // try set CORS headers manually isntead of using the cors package
+  // a la this post: https://stackoverflow.com/questions/72166644/node-and-heroku-error-no-access-control-allow-origin-header-is-present-on-th
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested, Content-Type, Accept Authorization");
+  next();
+});
+
+
 app.use(express.json());
 
 
