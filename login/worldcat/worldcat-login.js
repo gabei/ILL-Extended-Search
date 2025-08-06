c899577ef9d2a225586cc3b5c511e70f0057d196
gabei <gabe1792@gmail.com>
2025-08-06 15:23:22 -0500

    decrease element timeouts

--- login/worldcat/worldcat-login.js
+++ login/worldcat/worldcat-login.js
@@ -237,7 +237,7 @@ async function expandLibraryHoldingsList(){
 }
 
 
-async function waitForElementToAppearAndClick(selector, maxWaitTime = 30000) {
+async function waitForElementToAppearAndClick(selector, maxWaitTime = 5000) {
   // See documentation for waitForFunction:
   // https://pptr.dev/api/puppeteer.page.waitforfunction
 
